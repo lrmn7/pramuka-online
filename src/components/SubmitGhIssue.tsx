41b081d53e0b63de66247d7cce01219c2b20b571
L RMN <lrmnid@gmail.com>
2023-05-12 00:07:04 +0700

    first commit

---
+++ src/components/SubmitGhIssue.tsx
@@ -0,0 +1,18 @@
+function SubmitGhIssue() {
+  return (
+    <div className="mt-4 text-sm text-gray-600 dark:text-gray-100">
+      Tidak menemukan yang kamu cari? Kamu bisa membuat permintaan penambahan data baru melalui
+      {` `}
+      <a
+        href="https://is-a.fun/"
+        target="_blank"
+        rel="noopenner noreferrer"
+        className="text-link"
+      >
+        Sosial Link
+      </a>
+    </div>
+  )
+}
+
+export default SubmitGhIssue
